495db279be1617cd32561edd033f00cc701c3c99
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-22 17:14:01 -0500

    Syntax Correction
    -in bookmarks/models.py:
    - change class syntax to make "Models" be singular
    - test in migration command (pass)

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from uuid import uuid4
 
-class Bookmark(models.Models):
+class Bookmark(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     url = models.URLField('URL', unique=True)
     name = models.CharField(max_length=200)
