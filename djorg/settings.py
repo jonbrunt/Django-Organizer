4171d61a78ff413657874048c01bd839b0ad1ed2
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-23 17:34:07 -0500

    - Mask Secret Key
    - Install python-decouple as dependency
    - create env file in root
    - add SECRET_KEY and DEBUG file to env
    - add syntax to reference these from djorg/settings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^t92us_t2(=bz0k9x1%7xkufe!p@y9stg+1#j)4(0v@d6i=oaq'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
