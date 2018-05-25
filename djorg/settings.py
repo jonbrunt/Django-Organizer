272a3c1af31ae6f153a174ec0b5d00bbe015b6cf
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-25 16:29:36 -0500

    Procfile/Deployment Settings
    - in root:
    - add Procfile with proper syntax
    
    - in djorg/settings.py
    - import dj_database_url
    -configure ALLOWED_HOSTS
    - add whitenoise to middleware const
    - configure DATABASE_URL
    - configure DATABASES default key

--- djorg/settings.py
+++ djorg/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,6 +84,9 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = config('DATABASE_URL')
+
+DATABASES['default'] = dj_database_url.config(default=DATABASE_URL, conn_max_age=60)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
