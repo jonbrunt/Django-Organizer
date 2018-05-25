01e99350b419663c7e1fadea754a15117e75f88a
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-25 10:07:58 -0500

    Add Personal Bookmark
    -in bookmarks/models.py:
    - add PersonalBookmark class
    - inherit Bookmark to class
    - add user field
    
    - in bookmarks admin.py:
    - import and register PersonalBookmark

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
-admin.site.register(Bookmark)
+admin.site.register((Bookmark, PersonalBookmark))
