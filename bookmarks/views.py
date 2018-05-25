7a00f507c8abb1fcf4e3986607c38cd315967c0b
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-25 10:47:46 -0500

    Properly Display Personal Bookmarks
    - in bookmarks/views.py:
    - import PersonalBookmark
    - disable E1101 for block
    - exclude public view of any bookmarks with an id
    - conditional for anonymous user
    - render no personal bookmarks if anonymous
    - else render user bookmarks
    
    - in index.html:
    - add syntax to show personal bookmarks
    
    - test in browser (pass)

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
 # Create your views here.
+#pylint: disable=E1101
 def index(request):
-  context = {
-    'bookmarks': Bookmark.objects.all(), #pylint: disable=E1101
-  }
+  context = {}
+
+  pbid = PersonalBookmark.objects.values_list('id')
+
+  context['bookmarks'] = Bookmark.objects.exclude(id__in=pbid)
+
+  # context = {
+  #   'bookmarks': Bookmark.objects.exclude(id_in=pbid),
+  #   'personal_bookmarks': PersonalBookmark.objects.filter(user=request.user),
+  # }
+
+  if request.user.is_anonymous:
+    context['personal_bookmarks'] = PersonalBookmark.objects.none()
+  else:
+      context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
 
   return render(request, 'bookmarks/index.html', context)
