4fd63357074ac17a6532ddfc0be664f16c315aa0
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-30 14:15:44 -0500

    Add api.py
    in notes:
    - add api.py
    - add syntax for view and serializer for notes

---
+++ notes/api.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers, viewsets
+from .models import Note
+
+class NoteSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Note
+        fields = ('title', 'content')
+    
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+        serializer_class = NoteSerializer
+        queryset = Note.objects.all()
