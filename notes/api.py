4b8b37791ffaabf9ec62038c52357d57433a5110
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-30 14:20:59 -0500

    Syntax Fix
    - in notes/api.py:
    - remove bad indents in lines 12 & 13

--- notes/api.py
+++ notes/api.py
@@ -9,5 +9,5 @@ class NoteSerializer(serializers.HyperlinkedModelSerializer):
     
 class NoteViewSet(viewsets.ModelViewSet):
     serializer_class = NoteSerializer
-        serializer_class = NoteSerializer
-        queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
