6fd86359bd67e9b963f7c60d26d2d0c040aebfe2
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-30 14:56:52 -0500

    Add User To New Note
    - in notes/api.py:
    - need to add info manually (in the code)
    - create a routine to abstract user for new note

--- notes/api.py
+++ notes/api.py
@@ -1,8 +1,14 @@
+# pylint: disable=E1101
 from rest_framework import serializers, viewsets
 from .models import Note
 
 class NoteSerializer(serializers.HyperlinkedModelSerializer):
 
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = Note.objects.create(user=user, **validated_data)
+        return note
+        
     class Meta:
         model = Note
         fields = ('title', 'content')
