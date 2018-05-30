7d7b915d02a917fba19a0d9c470519bb0e7845d9
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-30 12:28:40 -0500

    Add Notes Model
    - in notes/models.py:
    - import uuid4
    - define data representation on the class

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    content = models.CharField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    
